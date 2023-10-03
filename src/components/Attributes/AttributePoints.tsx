b4874078880366566ad464d81ceb4b6b9063cbc5
Chris N <anull@proton.me>
2023-10-03 17:56:48 +0200

    feat: attribute point cost.

--- src/components/Attributes/AttributePoints.tsx
+++ src/components/Attributes/AttributePoints.tsx
@@ -1,15 +1,20 @@
-import { Stack } from "react-bootstrap";
+import { OverlayTrigger, Popover, Stack } from "react-bootstrap";
 import { useRecoilValue } from "recoil";
 
 import { FloatingText } from "@neverquest/components/FloatingText";
 import { IconDisplay } from "@neverquest/components/IconDisplay";
+import { IconImage } from "@neverquest/components/IconImage";
 import { useDeltaText } from "@neverquest/hooks/useDeltaText";
 import { ReactComponent as IconAttributePoints } from "@neverquest/icons/attribute-points.svg";
-import { attributePoints } from "@neverquest/state/attributes";
+import { ReactComponent as IconEssence } from "@neverquest/icons/essence.svg";
+import { attributePoints, level } from "@neverquest/state/attributes";
 import { deltas } from "@neverquest/state/deltas";
+import { formatValue } from "@neverquest/utilities/formatters";
+import { getAttributePointCost } from "@neverquest/utilities/getters";
 
 export function AttributePoints() {
   const attributePointsValue = useRecoilValue(attributePoints);
+  const levelValue = useRecoilValue(level);
 
   const deltaAttributePoints = deltas("attributePoints");
 
@@ -26,7 +31,23 @@ export function AttributePoints() {
         <FloatingText delta="attributePoints" />
       </Stack>
 
-      {attributePointsValue}
+      <OverlayTrigger
+        overlay={
+          <Popover>
+            <Popover.Header className="text-center">Attribute point cost</Popover.Header>
+
+            <Popover.Body>
+              <Stack className="justify-content-center" direction="horizontal" gap={1}>
+                <IconImage Icon={IconEssence} size="small" />
+
+                {formatValue({ value: getAttributePointCost(levelValue) })}
+              </Stack>
+            </Popover.Body>
+          </Popover>
+        }
+      >
+        <span>{attributePointsValue}</span>
+      </OverlayTrigger>
     </Stack>
   );
 }
